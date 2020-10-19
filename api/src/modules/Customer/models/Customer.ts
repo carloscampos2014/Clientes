99e4b5dbea9edd9d0f970bc5fb65f01d712738c5
Carlos Alberto de Lima Campos <carlosalbertode.campos@gmail.com>
2020-10-19 08:54:14 -0300

    Customer Create

---
+++ api/src/modules/Customer/models/Customer.ts
@@ -0,0 +1,57 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from "typeorm";
+import User from "../../User/models/User";
+
+@Entity("customers")
+export default class Customer {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  cellphone: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  zipcode: string;
+
+  @Column()
+  number: string;
+
+  @Column()
+  complement: string;
+
+  @Column()
+  address: string;
+
+  @Column()
+  district: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  state: string;
+
+  @Column()
+  country: string;
+
+  @Column()
+  obs: string;
+
+  @ManyToOne(() => User, (user) => user.customers)
+  @JoinColumn({ name: "user_id" })
+  user: User;
+}
